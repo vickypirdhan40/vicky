d28be8ec87576426ad128f85232e555176c1a218
vickypirdhan40 <vickystudio5000@gmail.com>
2025-09-19 20:44:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let index = 0;
-const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+const totalSlides = images.length;
 
-function showSlide(n) {
-  slides.forEach((img, i) => {
-    img.classList.toggle("active", i === n);
-  });
+function showSlide(index) {
+  if (index >= totalSlides) slideIndex = 0;
+  if (index < 0) slideIndex = totalSlides - 1;
+  slides.style.transform = `translateX(${-slideIndex * 800}px)`;
 }
 
-document.querySelector(".next").onclick = () => {
-  index = (index + 1) % slides.length;
-  showSlide(index);
-};
+document.querySelector('.next').addEventListener('click', () => {
+  slideIndex++;
+  showSlide(slideIndex);
+});
+document.querySelector('.prev').addEventListener('click', () => {
+  slideIndex--;
+  showSlide(slideIndex);
+});
 
-document.querySelector(".prev").onclick = () => {
-  index = (index - 1 + slides.length) % slides.length;
-  showSlide(index);
-};
-
-showSlide(index);
+// Auto slide
+setInterval(() => {
+  slideIndex++;
+  showSlide(slideIndex);
+}, 4000);
